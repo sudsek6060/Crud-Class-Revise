c4c5e4bc5df555da3b3b6e9b77efd83f8634d5d4
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2022-12-15 20:09:36 +0530

    Controllers created

---
+++ backend/controller/userController.js
@@ -0,0 +1,79 @@
+const User = require("../models/userModel")
+
+exports.home = (req, res) => {
+    res.send("Hello Alpha")
+};
+
+exports.createUser = async(req, res) => {
+    try {
+        const {name, email} = req.body;
+
+        //Check User Fieled all the details or not
+        if (!name || !email) {
+            throw new Error("Name and Email are required")
+        }
+        //If user already created
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            throw new Error("email already exists try with another email")
+        }
+        //Inserting User to Database
+        const user = await User.create({name, email})
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(201).json({
+            success: true,
+            users,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
+
+exports.editUser = async(req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body);
+        res.status(201).json({
+            success: true,
+            message: "User updated successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: error.message
+        })
+    }
+};
+
+exports.deleteUser = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await User.findByIdAndDelete(userId);
+        res.status(201).json({
+            success: true,
+            message: "User deleted successfully"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
